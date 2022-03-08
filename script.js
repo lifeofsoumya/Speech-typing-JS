18c8f0eb89a27f2107986b76993ceeac6ac6a3af
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-03-08 22:40:43 +0530

    hidden visible onclick for 'speak', eventlistener 2 function execution.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const voiceBtn = document.getElementById('voice-btn')
 
-voiceBtn.addEventListener("click", addAnim)
+voiceBtn.addEventListener("click", () => {    
+    addAnim();
+    convertAudio();    
+});
 
 function addAnim(){
     voiceBtn.classList.add("anim");
+    document.getElementById('speak-h1').classList.remove("hidden");
+}
+
+function convertAudio(){
+const texts = document.querySelector(".texts");
 }
